89084bfe016aa545dbb27013a3aca318f867a486
kevindotklein <kevindotklein@gmail.com>
2023-04-30 17:27:05 -0300

    test: test profile

--- src/test/java/com/kevindotklein/springbootessentials/SpringbootessentialsApplicationTests.java
+++ src/test/java/com/kevindotklein/springbootessentials/SpringbootessentialsApplicationTests.java
@@ -3,10 +3,10 @@ package com.kevindotklein.springbootessentials;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringbootessentialsApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
