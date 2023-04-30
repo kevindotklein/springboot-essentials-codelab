8bc2f2a20288b35eac84b8ff5cbe2380c3759468
kevindotklein <kevindotklein@gmail.com>
2023-04-30 09:55:05 -0300

    feat: pagination

--- src/main/java/com/kevindotklein/springbootessentials/controller/AnimeController.java
+++ src/main/java/com/kevindotklein/springbootessentials/controller/AnimeController.java
@@ -9,6 +9,8 @@ import com.kevindotklein.springbootessentials.util.DateUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,8 +27,8 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public ResponseEntity<List<Anime>> getAll(){
-        return ResponseEntity.ok(this.animeService.findAll());
+    public ResponseEntity<Page<Anime>> getAll(Pageable pageable){
+        return ResponseEntity.ok(this.animeService.findAll(pageable));
     }
 
     @GetMapping("/{id}")
--- src/main/java/com/kevindotklein/springbootessentials/service/AnimeService.java
+++ src/main/java/com/kevindotklein/springbootessentials/service/AnimeService.java
@@ -4,10 +4,10 @@ import com.kevindotklein.springbootessentials.domain.Anime;
 import com.kevindotklein.springbootessentials.exception.BadRequestException;
 import com.kevindotklein.springbootessentials.repository.AnimeRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -17,8 +17,8 @@ public class AnimeService {
 
     private final AnimeRepository animeRepository;
 
-    public List<Anime> findAll(){
-        return this.animeRepository.findAll();
+    public Page<Anime> findAll(Pageable pageable){
+        return this.animeRepository.findAll(pageable);
     }
 
     public Anime findById(Long id){
